a7c7a171ca8c4390cbe5a37fd5dd2e13ea6d324f
sdoro <sandro.doro@gmail.com>
2018-05-28 11:09:35 +0200

    cleanup

--- app.py
+++ app.py
@@ -2,19 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
-app.secret_key="veryveryverysecrererevf"
-
 @app.route('/')
 def hello_world():
     return redirect(url_for('index'))
 
 @app.route('/index')
 def index():
-    username = session.get('username','')
-    if username != '':
-        alarms = userdb.all_alarms(session['user_id'])
-    else:
-        alarms = None
     return render_template("index.html", name=username, alarms=alarms)
 
 @app.route('/about')
